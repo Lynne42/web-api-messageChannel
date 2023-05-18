d3e490db2d25534fa025e6cfb371fc457ca4131c
qiaoling.niu <qiaoling.niu@realai.ai>
2023-05-18 14:21:46 +0800

    test

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-console.log('Hello!');
-window.addEventListener('DOMContentLoaded', () => {
-  const iframe1 = document.getElementById('iframe1');
-  const iframe2 = document.getElementById('iframe2');
+window.addEventListener("load", () => {
+  const iframe1 = document.getElementById("iframe1");
+  const iframe2 = document.getElementById("iframe2");
 
   const channel1 = new MessageChannel();
   const channel2 = new MessageChannel();
 
-  // 在 iframe1 中发送消息给 iframe2
-  iframe1.contentWindow.postMessage('Hello from iframe1', '*', [
-    channel1.port2,
-  ]);
+  // 监听 channel1.port1 接收到的信息
 
-  // 在 iframe2 中发送消息给 iframe1
-  iframe2.contentWindow.postMessage('Hello from iframe2', '*', [
-    channel2.port2,
-  ]);
+  const channel1Port1Onmessage = (event) => {
+    console.log("channel1.port1 onmessage: ", event.data);
 
-  channel1.port1.onmessage = (event) => {
-    console.log('Message received in iframe1:', event.data);
+    // channel2.port2.postMessage({
+    //   channel: "channel2",
+    //   message: "channel2 转发来自iframe2的消息",
+    // });
   };
 
-  channel2.port1.onmessage = (event) => {
-    console.log('Message received in iframe2:', event.data);
+  const channel2Port1Onmessage = (event) => {
+    console.log("channel2.port1 onmessage: ", event.data);
+
+    // channel1.port2.postMessage({
+    //   channel: "channel1",
+    //   message: "channel1 转发来自iframe2的消息",
+    // });
   };
+
+  iframe1.addEventListener("load", function () {
+    channel1.port1.onmessage = channel1Port1Onmessage;
+    iframe1.contentWindow.postMessage(
+      {
+        channel: "channel1",
+      },
+      "*",
+      [channel1.port2]
+    );
+  });
+
+  iframe2.addEventListener("load", function () {
+    channel2.port1.onmessage = channel2Port1Onmessage;
+    iframe2.contentWindow.postMessage(
+      {
+        channel: "channel2",
+      },
+      "*",
+      [channel2.port2]
+    );
+  });
 });
